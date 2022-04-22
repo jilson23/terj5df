027775875b5642beaaddd7076db6a972f7277cfd
jilson jimenez <jilsonjimenez@gmail.com>
2022-04-22 10:58:23 -0500

    fixin change input and delete value in input

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      firstName: '',
-      lastName: '',
-      invitados: [
-        {
-          nombre: 'Juan',
-          apellido: 'Perez'
-        }
-      ]
+      'first-name': '',
+      'last-name': '',
+      guest: []
     }
   }
 
@@ -30,9 +25,9 @@ class App extends Component {
   handleSubmit(event) {
     event.preventDefault();
     this.setState({
-      invitados: this.state.invitados.concat({
-        nombre: this.state.firstName,
-        apellido: this.state.lastName
+      guest: this.state.guest.concat({
+        nombre: this.state['first-name'],
+        apellido: this.state['last-name']
       })
     });
   }
@@ -45,12 +40,12 @@ class App extends Component {
             <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" value={this.state.firstName} onChange={this.handleChange} name="firstName" />
+                <input type="text" className="form-control" onChange={this.handleChange} name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" value={this.state.lastName} onChange={this.handleChange} name="lastName" />
+                <input type="text" className="form-control" onChange={this.handleChange} name="last-name" />
               </div>
 
               <div className="action">
@@ -67,7 +62,7 @@ class App extends Component {
               </thead>
               <tbody>
                   {
-                    this.state.invitados.map((invitado, index) => {
+                    this.state.guest.map((invitado, index) => {
                       return (
                         <tr key={index}>
                           <td>{invitado.nombre}</td>
